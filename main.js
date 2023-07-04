27a143c11a31cf36594786f6594e67df37bb6bea
Kate <violetds0826@gmail.com>
2023-07-04 15:20:42 -0400

    add phpemailer

--- main.js
+++ main.js
@@ -74,3 +74,34 @@ for (let i = 0; i < tagsButtons.length; i++) {
     })
 }
 
+//Modal
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector(".overlay")
+const openModalBtn = document.querySelector(".btn-open")
+const closeModalBtn = document.querySelector(".btn-close")
+
+const openModal = () => {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
+openModalBtn.addEventListener('click', openModal)
+
+const closeModal = () => {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+closeModalBtn.addEventListener('click', closeModal)
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener('keydown')
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        modalClose()
+    }
+
+})
