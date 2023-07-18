87b98e7af1bc07904b4d8d76e9b5d52c5e00d0f7
Kate <violetds0826@gmail.com>
2023-07-18 12:30:10 -0400

    changed services part

--- main.js
+++ main.js
@@ -120,8 +120,43 @@ for (const s of slider) {
 }
 
 
+// Carousel
+var multipleCardCarousel = document.querySelector(
+    "#carouselExampleControls"
+);
+var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+    interval: 2000,
+    wrap: false
+})
+if (window.matchMedia("(min-width: 768px)").matches) {
+    var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+        interval: false,
+    });
+    var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+    var cardWidth = $(".carousel-item").width();
+    var scrollPosition = 0;
+    $("#carouselExampleControls .carousel-control-next").on("click", function () {
+        if (scrollPosition < carouselWidth - cardWidth * 4) {
+            scrollPosition += cardWidth;
+            $("#carouselExampleControls .carousel-inner").animate(
+                { scrollLeft: scrollPosition },
+                600
+            );
+        }
+    });
+    $("#carouselExampleControls .carousel-control-prev").on("click", function () {
+        if (scrollPosition > 0) {
+            scrollPosition -= cardWidth;
+            $("#carouselExampleControls .carousel-inner").animate(
+                { scrollLeft: scrollPosition },
+                600
+            );
+        }
+    });
+} else {
+    $(multipleCardCarousel).addClass("slide");
+}
 
 
 
-
-
+i
